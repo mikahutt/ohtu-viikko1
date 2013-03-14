6f5b38d6484a00eb77a7f1f56714647b2a27a368
Mika Huttunen <mika.huttunen@helsinki.fi>
2013-03-14 12:49:49 +0200

    ja vielä rikottu

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -100,6 +100,7 @@ public class VarastoTest {
     @Test
     public void eiOtetaNegatiivista() {
         varasto.otaVarastosta(-2);
-        assertTrue(varasto.getSaldo() == 0);
+        //assertTrue(varasto.getSaldo() == 0);
+        assertTrue(varasto.getSaldo() == 2);
     }
 }
